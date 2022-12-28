4a4864c709a56571a1b379d154d80c6fbe4e04c4
weejinyoung <a8118199@gmail.com>
2022-12-28 21:27:50 +0900

    [ init ] for test my spring setting

---
+++ test-wak/src/main/java/latenightrestaurant/testwak/TestWakApplication.java
@@ -0,0 +1,13 @@
+package latenightrestaurant.testwak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWakApplication.class, args);
+	}
+
+}
---
+++ test-wak/src/test/java/latenightrestaurant/testwak/TestWakApplicationTests.java
@@ -0,0 +1,13 @@
+package latenightrestaurant.testwak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
