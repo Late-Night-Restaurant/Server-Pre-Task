bb9e446080d9f543eaae76cbb8691acc71f40ba0
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-01-09 02:34:00 +0900

    [refactor] : user Entity 수정

---
+++ src/main/java/com/backend/oauthlogin/entity/Role.java
@@ -0,0 +1,6 @@
+package com.backend.oauthlogin.entity;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- src/main/java/com/backend/oauthlogin/entity/User.java
+++ src/main/java/com/backend/oauthlogin/entity/User.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity    // @Entity 어노테이션: 자동으로 JPA 연동
 @Table(name = "`USER`")
@@ -13,12 +14,15 @@ import java.util.Set;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BaseTimeEntity implements UserDetails {
 
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Column(name = "username", length = 50, unique = true)
     private String username;
@@ -38,4 +42,25 @@ public class User {
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
     private Set<Authority> authorities;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
+// Profile -> 연관관계 매핑 고려해서 클래스 생성할 예정
