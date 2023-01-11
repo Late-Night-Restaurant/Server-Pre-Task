a8db78fe2f56932c14a11ee5638f1b7feebf1b5f
weejinyoung <a8118199@gmail.com>
2023-01-11 14:26:48 +0900

    [ etc ] 기타 등등 dto 수정
    Account와 Authority 삭제

--- src/main/java/com/backend/oauthlogin/dto/UserDto.java
+++ src/main/java/com/backend/oauthlogin/dto/UserDto.java
@@ -1,13 +1,11 @@
 package com.backend.oauthlogin.dto;
 
-import com.backend.oauthlogin.entity.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Getter @Setter
 @Builder
@@ -15,32 +13,12 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class UserDto {
 
+    @Email
     @NotNull
-    @Size(min = 3, max = 50)
-    private String username;
+    private String email;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull
     @Size(min = 3, max = 100)
     private String password;
-
-    @NotNull
-    @Size(min = 3, max = 50)
-    private String nickname;
-
-    private Set<AuthorityDto> authorityDtoSet;
-
-    public static UserDto from(User user) {
-        if (user == null) return null;
-
-        return UserDto.builder()
-                .username(user.getUsername())
-                .nickname(user.getNickname())
-                .authorityDtoSet(user.getAuthorities().stream()
-                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
-                        .collect(Collectors.toSet()))
-                .build();
-
-    }
-
 }
