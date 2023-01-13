991566c18a1470c85d302019cf087ce87ef7d150
weejinyoung <a8118199@gmail.com>
2023-01-13 22:46:46 +0900

    [ fix ] conflict 해결2
    exception 패키지 생성

---
+++ src/main/java/com/backend/oauthlogin/exception/BaseException.java
@@ -0,0 +1,11 @@
+package com.backend.oauthlogin.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class BaseException extends Exception{
+    private BaseResponseStatus status;
+}
